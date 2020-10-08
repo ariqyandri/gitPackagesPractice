5f966586b0a94c1cb55036e812d491087f82be34
ariqyandri <ariq140199@gmail.com>
2020-10-08 12:02:47 +0200

    End of packages practice

---
+++ index.js
@@ -0,0 +1,8 @@
+const toTitleCase = require('to-title-case');
+// console.log(toTitleCase);
+
+const lower = 'The Lord of the Rings: The Return of the King';
+const titlePage = toTitleCase(lower);
+console.log(titlePage);
+
+
